7b885baf51c0678f45d2aae1abc86aef8aed2de3
Adrian Carp <114384882+Sovan0@users.noreply.github.com>
2024-11-05 20:32:07 +0200

    main

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -71,6 +71,6 @@ class PostController extends Controller
     {
         $post->delete();
 
-        return redirect('/');
+        return redirect('/')->with('message', 'The post was deleted!');
     }
 }
